049f11c814b31f280523582b19d1ef1ca0e3a676
qr22020817 <395543600@qq.com>
2019-08-16 14:27:50 +0800

    我的五星红旗项目N2

--- turtle_五星红旗.py
+++ turtle_五星红旗.py
@@ -7,7 +7,7 @@ t = turtle.Pen()
 # 确定我们最下边小星星的位置
 t.down()
 t.color('red')
-t.goto(-200,40)
+t.goto(-185,50)
 t.up()
 t.color('yellow', 'yellow')
 # 第一个小星星
@@ -22,7 +22,7 @@ t.up()
 #第二个星星
 t.left(90)
 t.right(90)
-t.goto(-198,250)
+t.goto(-185,274)
 t.down()
 t.begin_fill()
 for i in range(5):
@@ -32,7 +32,7 @@ t.end_fill()
 t.up()
 # 第三个星星
 t.left(30)
-t.goto(-150,90)
+t.goto(-128,100)
 t.down()
 t.begin_fill()
 for i in range(5):
@@ -42,7 +42,7 @@ t.end_fill()
 t.up()
 # 第四个星星
 t.left(30)
-t.goto(-150,160)
+t.goto(-128,180)
 t.down()
 t.begin_fill()
 for i in range(5):
@@ -52,11 +52,12 @@ t.end_fill()
 t.up()
 # 第五个大星星
 t.right(30)
-t.goto(-400,160)
+# 起始点(位置靠左第二个小格，靠上)
+t.goto(-410,192)
 t.down()
 t.begin_fill()
 for i in range(5):
-    t.forward(150)
+    t.forward(180)
     t.right(144)
 t.end_fill()
 t.up()
